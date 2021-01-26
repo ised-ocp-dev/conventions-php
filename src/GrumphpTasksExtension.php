472d2f2c4bb3799909223db0c247785f4ad1ce0c
william roboly <william.roboly@appnovation.com>
2021-01-26 12:02:54 -0500

    Update grumphp yaml file and class TaskExtension

--- src/GrumphpTasksExtension.php
+++ src/GrumphpTasksExtension.php
@@ -15,55 +15,30 @@ use Symfony\Component\DependencyInjection\Exception\RuntimeException;
  */
 class GrumphpTasksExtension implements ExtensionInterface
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function load(ContainerBuilder $container): void
-    {
-        if ($container->hasParameter('skip_tasks')) {
-            $tasks = $container->getParameter('tasks');
-            $skip_tasks = $container->getParameter('skip_tasks');
-
-            foreach ($skip_tasks as $name) {
-                unset($tasks[$name]);
-            }
-
-            $container->setParameter('tasks', $tasks);
-
-            if ($container->hasParameter('testsuites')) {
-                $testsuites = $container->getParameter('testsuites');
-
-                foreach ($testsuites as $id => $data) {
-                    $testsuites[$id]['tasks'] = array_filter(
-                        $testsuites[$id]['tasks'],
-                        static function (string $task) use ($skip_tasks): bool {
-                            return !in_array($task, $skip_tasks, true);
-                        }
-                    );
-
-                    if ($testsuites[$id]['tasks'] === []) {
-                        unset($testsuites[$id]);
-                    }
-                }
-
-                $container->setParameter('testsuites', $testsuites);
-            }
+  /**
+  * {@inheritdoc}
+  */
+  public function load(ContainerBuilder $container): void
+  {
+    if ($container->hasParameter('extra_tasks')) {
+      $tasks = $container->getParameter('tasks');
+      foreach ($container->getParameter('extra_tasks') as $name => $value) {
+        if (array_key_exists($name, $tasks)) {
+          throw new RuntimeException("Cannot override already defined task '{$name}' in 'extra_tasks'.");
         }
-
-        if ($container->hasParameter('extra_tasks')) {
-            $tasks = $container->getParameter('tasks');
-
-            foreach ($container->getParameter('extra_tasks') as $name => $value) {
-                if (isset($tasks[$name])) {
-                    throw new RuntimeException(
-                        sprintf("Cannot override already defined task '%s' in 'extra_tasks'", $name)
-                    );
-                }
-
-                $tasks[$name] = $value;
-            }
-
-            $container->setParameter('tasks', $tasks);
-        }
+        $tasks[$name] = $value;
+      }
+      $container->setParameter('tasks', $tasks);
     }
+
+    if ($container->hasParameter('skip_tasks')) {
+      $tasks = $container->getParameter('tasks');
+
+      foreach ($container->getParameter('skip_tasks') as $name) {
+        unset($tasks[$name]);
+      }
+
+      $container->setParameter('tasks', $tasks);
+    }
+  }
 }
